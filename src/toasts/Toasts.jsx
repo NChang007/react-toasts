67075a73f873bb3024b8564ea31197e1af69f7aa
Nelson Chang <nelsonc924@gmail.com>
2023-10-19 19:44:07 -0400

    simple update

--- src/toasts/Toasts.jsx
+++ src/toasts/Toasts.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import "./toasts.css"
 
 const Toasts = () => {
+    // how the fuck do i set up the toast and where uts us sypposed to be 
+    // I need the where its going to go
+    // The action of coming out and other stuff
   return (
-    <div>Toasts</div>
+    <div>
+        <div className='toast'>
+            This is the text that shows
+            <span className='closebtn'>X</span>
+        </div>
+    </div>
   )
 }
 
